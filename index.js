3f114c418fc1b9884663e2ace6ea075661f51c35
Pranav Teegavarapu <pranavnt@outlook.com>
2020-11-27 01:42:40 -0800

    basic redirect works

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 const app = require("express")();
+
+const PORT = 4567;
+
+// put your username here
+const USERNAME = "pranavnt";
+
+app.get("/:repo", function (req, res) {
+  res.redirect("https://github.com/" + USERNAME + req.params.repo);
+});
+
+app.listen(PORT, () => console.log(`App listening at port ${PORT}`));
